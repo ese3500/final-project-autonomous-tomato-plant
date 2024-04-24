8e0aa2d2c5bcce681beab2578658a031e9a8f875
JiaxiaoGabrielZhangEE <157425369+JiaxiaoGabrielZhangEE@users.noreply.github.com>
2024-04-24 18:28:22 -0400

    Update motor_control.h

--- motor_control.h
+++ motor_control.h
@@ -8,5 +8,8 @@ extern void forward(void);
 extern void reverse(void);
 extern void left(void);
 extern void right(void);
+extern void stop(void);
+extern void drift_left(void);
+extern void drift_right(void);
 
 #endif // MOTOR_CONTROL_H
